29d9b80c35ba6a5d61e1bb66738beb0eddb87445
alonsofabila <jorgealonsofabila@gmail.com>
2024-04-22 23:26:49 -0600

    (chore): Django Rest Framework set up.

--- task_app/settings.py
+++ task_app/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'task_app.apps.tasks.apps.TasksConfig',
     'task_app.apps.users.apps.UsersConfig',
 ]
@@ -48,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -131,3 +134,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cors authorized domains
+CORS_ALLOWED_ORIGINS = []
