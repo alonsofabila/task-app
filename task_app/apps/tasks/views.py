a537b6737d09c3bb05734142247090564d60c399
alonsofabila <jorgealonsofabila@gmail.com>
2024-05-11 18:55:53 -0600

    (feature): Endpoint to update task created.

--- task_app/apps/tasks/views.py
+++ task_app/apps/tasks/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializer import TasksSerializer
 from .models import Tasks
@@ -16,7 +17,25 @@ class TasksViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         if serializer.is_valid():
+
             serializer.validated_data['id'] = generate_custom_uuid('task')
             serializer.save(created_by=self.request.user)
+
         else:
             print(serializer.errors)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+
+        if serializer.is_valid():
+
+            if instance.created_by != request.user:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+
+            serializer.save()
+
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
--- task_app/apps/users/urls.py
+++ task_app/apps/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from task_app.apps.users.views import UserViewSet
+from task_app.apps.users.views import UserViewSet, DetailUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     path('api/v1/user/register/', UserViewSet.as_view(), name="register"),
+    path('api/v1/user/<int:pk>/', DetailUserView.as_view(), name="user"),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/api-auth/', include('rest_framework.urls')),
